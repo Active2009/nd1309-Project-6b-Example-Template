aff547ea9619aaabc7ea77ce85b6bce7577c3278
Active2009 <55351072+Active2009@users.noreply.github.com>
2020-05-20 12:57:06 -0400

    Project 3 Coffee Supply

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,9 +1,18 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      host: "localhost",
+      provider: function() {
+        return new HDWalletProvider( "supply execute absorb bottom caution ecology science finger fatal ignore climb affair", "https://rinkeby.infura.io/v3/16c5f41b37634654a28e807565066eca");
+      },
+      network_id:4
     }
   }
 };
